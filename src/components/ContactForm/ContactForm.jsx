e5f3ec1a5c7e7b82b9215378cb2db4d95c6e92f0
MikNick <mikita.krylev@gmail.com>
2024-05-06 22:13:51 +0300

    done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,36 +1,53 @@
-import { Formik,Form,Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { useId } from "react";
-import css from "./ContactForm.module.css"
+import css from "./ContactForm.module.css";
+
 const initialValues = {
   name: "",
   number: ""
 };
-function ContactForm({addNewContact}) {
 
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("Name is required")
+    .min(3, "Name must be at least 3 characters")
+    .max(50, "Name must be at most 50 characters"),
+  number: Yup.string()
+    .required("Phone number is required")
+    .min(3, "Phone number must be at least 3 characters")
+    .max(50, "Phone number must be at most 50 characters"),
+});
+
+function ContactForm({ addNewContact }) {
   const nameFieldId = useId();
   const phoneFieldId = useId();
 
   const handleSubmit = (values, actions) => {
     console.log(values);
     actions.resetForm();
-    addNewContact(values)
+    addNewContact(values);
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
       <Form className={css.form}>
-        <label htmlFor={nameFieldId}>Username</label>
+        <label htmlFor={nameFieldId}>Name</label>
         <Field type="text" name="name" id={nameFieldId} />
+        <ErrorMessage name="name" component="div" className={css.error} />
 
         <label htmlFor={phoneFieldId}>Phone</label>
-        <Field type="phone" name="number" id={phoneFieldId} />
+        <Field type="text" name="number" id={phoneFieldId} />
+        <ErrorMessage name="number" component="div" className={css.error} />
 
         <button type="submit">Add new contact</button>
       </Form>
     </Formik>
   );
-  }
-  
-  export default ContactForm;
+}
 
-
+export default ContactForm;
